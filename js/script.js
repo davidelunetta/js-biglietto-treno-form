685ca5e9d68c047f4cfd07f45ca5bb16b6b8c2ab
davidelunetta <davidelunetta.27@gmail.com>
2023-10-10 14:19:45 +0200

    added function

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function calcolaPrezzo() {
+    const km = parseFloat(document.getElementById("kmInput").value);
+    const age = parseInt(document.getElementById("ageInput").value);
+
+    const prezzoBase = km * 0.21;
+    let prezzoFinale = prezzoBase;
+
+    if (age < 18) {
+        // Applica lo sconto del 20% per i minorenni
+        prezzoFinale *= 0.8;
+    } else if (age >= 65) {
+        // Applica lo sconto del 40% per gli over 65
+        prezzoFinale *= 0.6;
+    }
+
+    document.getElementById("ticketPrice").textContent = `Prezzo del biglietto: ${prezzoFinale.toFixed(2)} €`;
+}
