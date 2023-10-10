faf9de9030015883b1398682d37fb65251987b5d
davidelunetta <davidelunetta.27@gmail.com>
2023-10-10 23:24:05 +0200

    modified offcanvas

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 function calcolaPrezzo() {
     let km = parseFloat(document.getElementById("kmInput").value);
     let age = parseInt(document.getElementById("ageInput").value);
+    let nome = document.getElementById("nomeInput").value;
+    let cognome = document.getElementById("cognomeInput").value;
     let prezzoBase = km * 0.21;
     let prezzoFinale = prezzoBase;
+    let x = parseInt(Math.random() * 100) ;
+    let y = parseInt(Math.random() * 100000) ;
+    console.log(x)
     
     if (age < 18) {
         // Applica lo sconto del 20% per i minorenni
@@ -12,5 +17,9 @@ function calcolaPrezzo() {
         prezzoFinale *= 0.6;
     }
 
+    document.getElementById("nome").textContent = `Intestatario: ${nome} ${cognome}`;
     document.getElementById("ticketPrice").textContent = `Prezzo: ${prezzoFinale.toFixed(2)} €`;
+    document.getElementById("vagone").textContent = `Vagone: ${x}`;
+    document.getElementById("codice-cp").textContent = `Codice CP: ${y}`;
+    document.getElementById("offerta").textContent = `Offerta: ${age}`
 }
